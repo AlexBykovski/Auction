4701101a84403fa8c7407c104f393a6753df6d88
AlexBykovski <bykovski.work@gmail.com>
2018-03-10 15:23:41 +0300

    add upload images for Goods

---
+++ src/Upload/FileUpload.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Upload;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Gaufrette\Filesystem;
+
+class FileUpload
+{
+    const GENERAL = 'general';
+    const GOODS = 'goods';
+
+    private static $allowedMimeTypes = array(
+        'image/jpeg',
+        'image/jpg',
+        'image/png',
+        'image/gif'
+    );
+
+    private $filesystem;
+
+    private $folder;
+
+    public function __construct(Filesystem $filesystem)
+    {
+        $this->filesystem = $filesystem;
+    }
+
+    public function setFolder($folder){
+        $this->folder = $folder;
+    }
+
+    public function getFolder(){
+        return $this->folder ?: $folder = self::GENERAL;
+    }
+
+    public function upload(UploadedFile $file)
+    {
+        if (!in_array($file->getClientMimeType(), self::$allowedMimeTypes)) {
+            throw new \InvalidArgumentException(sprintf('Files of type %s are not allowed.', $file->getClientMimeType()));
+        }
+
+        $filename = sprintf('%s/%s/%s/%s.%s', $this->getFolder(), date('Y'), date('m'), uniqid(), $file->getClientOriginalExtension());
+        $adapter = $this->filesystem->getAdapter();
+        $adapter->write($filename, file_get_contents($file->getPathname()));
+        return $filename;
+    }
+}
