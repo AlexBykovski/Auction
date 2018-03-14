453b948e9d0be7b6dcfe15fb3aa76fe5035f836f
AlexBykovski <bykovski.work@gmail.com>
2018-03-14 18:14:25 +0300

    add login temp

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('public/login.html.twig', []);
+    }
+}
