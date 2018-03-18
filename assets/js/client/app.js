80b024f075669659ae6df8cedfcf9732f6e5037b
AlexBykovski <bykovski.work@gmail.com>
2018-03-18 20:13:21 +0300

    add angular

---
+++ assets/js/client/app.js
@@ -0,0 +1,13 @@
+//var app = angular.module('app',['ngSanitize']);
+//var appAuction = angular.module('appAuction', ['']);
+
+//appAuction.controller('BonusController', ['$scope', function($scope) {
+//}]);
+
+(function(angular) {
+    'use strict';
+    window.appAuction = angular.module('appAuction', ['ngSanitize']);
+
+    appAuction.controller('BonusController', ['$scope', function($scope) {
+    }]);
+})(window.angular);
