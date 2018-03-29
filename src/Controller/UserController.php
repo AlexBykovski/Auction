15faf2686948facc2b3e9c0a0f62a06d9f39cfe1
AlexBykovski <bykovski.work@gmail.com>
2018-03-29 13:57:06 +0300

    add redirect if superadmin

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class UserController extends Controller
 {
     /**
      * @Route("/my-auctions", name="profile_my_auctions")
+     *
+     * @Security("has_role('ROLE_USER')")
      */
     public function myAuctionsShowAction(Request $request)
     {
+        if($this->isGranted("ROLE_SUPER_ADMIN")){
+            return $this->redirectToRoute("list_products");
+        }
+
         return $this->render('client/profile/my-auctions.html.twig', []);
     }
 }
