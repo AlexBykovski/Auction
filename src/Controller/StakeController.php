7992af3a97cc48e7f86e01398a457490c9aa34e7
AlexBykovski <bykovski.work@gmail.com>
2018-04-07 23:19:39 +0300

    add buy stakes page

--- src/Controller/StakeController.php
+++ src/Controller/StakeController.php
@@ -4,8 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Entity\StakeExpense;
+use App\Entity\StakeOffering;
+use App\Entity\StakePurchase;
 use App\Entity\User;
+use App\Form\Type\BuyStakesType;
+use App\Helper\StakeHelper;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -51,6 +56,67 @@ class StakeController extends BaseController
 
         return new JsonResponse([
             "success" => true,
+        ]);
+    }
+
+    /**
+     * @Route("/buy-stakes", name="buy_stakes")
+     */
+    public function buyStakesAction(Request $request, StakeHelper $stakeHelper)
+    {
+        if($this->isGranted("ROLE_SUPER_ADMIN") || !$this->isGranted("ROLE_USER")){
+            return $this->redirectToRoute("list_products");
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        /** @var User $user */
+        $user = $this->getUser();
+        $simpleStakes = $stakeHelper->getStakesArrayForBuyAction();
+        $specialStakes = $stakeHelper->getStakesArrayForBuyAction(true);
+
+        $form = $this->createForm(BuyStakesType::class, [
+            "simpleStakes" => array_values($simpleStakes),
+            "specialStakes" => array_values($specialStakes),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $paymentType = $form->get("payment")->getData();
+            $selectedSimpleStakes = $form->get("simpleStakes")->getData()->getValues();
+            $selectedSpecialStakes = $form->get("specialStakes")->getData()->getValues();
+
+            if(!(count($selectedSimpleStakes) + count($selectedSpecialStakes))){
+                $form->get("specialStakes")->addError(new FormError("Вы должны выбрать минимум 1 пакет для продолжения"));
+            }
+            else{
+                $count = $stakeHelper->getCountForBuyAction($selectedSimpleStakes, $selectedSpecialStakes);
+                $cost = $stakeHelper->getCostForBuyAction($selectedSimpleStakes, $selectedSpecialStakes);
+
+                //@@todo add payment logic, and then remove this block
+                //start block
+                $stakeDetail = $user->getStakeDetail();
+                $stakePurchase = new StakePurchase();
+                $stakePurchase->setCost($cost);
+                $stakePurchase->setCount($count);
+                $stakePurchase->setStakeDetail($stakeDetail);
+                $stakeDetail->setCount($stakeDetail->getCount() + $count);
+
+                $em->persist($stakePurchase);
+                $em->flush();
+                //end block
+
+                $form = $this->createForm(BuyStakesType::class, [
+                    "simpleStakes" => array_values($simpleStakes),
+                    "specialStakes" => array_values($specialStakes),
+                ]);
+            }
+        }
+
+        return $this->render('client/stake/buy_stakes.html.twig', [
+            "form" => $form->createView(),
+            "simpleStakes" => $simpleStakes,
+            "specialStakes" => $specialStakes,
         ]);
     }
 }
