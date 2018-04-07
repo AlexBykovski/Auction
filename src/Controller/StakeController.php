74a55b246577a19078ebdbd7d9e70b7ddbc74d06
AlexBykovski <bykovski.work@gmail.com>
2018-04-07 23:21:50 +0300

    remove unused code

--- src/Controller/StakeController.php
+++ src/Controller/StakeController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Entity\StakeExpense;
-use App\Entity\StakeOffering;
 use App\Entity\StakePurchase;
 use App\Entity\User;
 use App\Form\Type\BuyStakesType;
