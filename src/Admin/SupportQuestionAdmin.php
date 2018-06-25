e4038beceb8f1c6fede56039d0ec2c0cc1672c86
AlexBykovski <bykovski.work@gmail.com>
2018-06-26 00:01:49 +0300

    add fullname to admin

--- src/Admin/SupportQuestionAdmin.php
+++ src/Admin/SupportQuestionAdmin.php
@@ -11,7 +11,8 @@ class SupportQuestionAdmin extends AbstractAdmin
 {
     protected function configureListFields(ListMapper $listMapper)
     {
-        $listMapper->addIdentifier('user.id', TextType::class, ['label' => 'ID пользователя', 'sortable' => false, 'editable' => false]);
+        $listMapper->addIdentifier('user.username', TextType::class, ['label' => 'Ник', 'sortable' => false, 'editable' => false]);
+        $listMapper->addIdentifier('user.fullName', TextType::class, ['label' => 'ФИО', 'sortable' => false, 'editable' => false]);
         $listMapper->addIdentifier('question', TextType::class, ['label' => 'Вопрос', 'sortable' => false]);
         $listMapper->addIdentifier('image', null, ['label' => "Изображение", 'template' => 'admin/support_question_image.html.twig', 'sortable' => false]);
         $listMapper->addIdentifier('createdAt', 'datetime', ['label' => "Дата", "format" => "d-m-Y", 'sortable' => false]);
